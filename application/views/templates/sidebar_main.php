9a34145f2d618710c833616fe2f0aae7c81ce22a
Taupik <taufikbjm24@gmail.com>
2023-04-26 15:36:57 +0800

    Update Sidebar

--- application/views/templates/sidebar_main.php
+++ application/views/templates/sidebar_main.php
@@ -12,20 +12,34 @@
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
-
+        <style>
+            .marg {
+                margin-top: 7px;
+            }
+        </style>
         <!-- sidebar menu: : style can be found in sidebar.less -->
-        <ul class="sidebar-menu" data-widget="tree">
+        <ul class="sidebar-menu marg" data-widget="tree">
             <li class="header">MAIN NAVIGATION</li>
 
-            <li class="<?php if ($page == "Dashboard") {echo "active";}?>"><a href="<?php echo base_url() . 'dashboard'; ?>"><i class="fa fa-tachometer"></i> <span>Dashboard</span></a></li>
-            <li class="<?php if ($page == "Wisata") {echo "active";}?>"><a href="<?php echo base_url() . 'wisata'; ?>"><i class="fa fa-suitcase"></i> <span>Wisata</span></a></li>
+            <li class="<?php if ($page == "Dashboard") {
+                            echo "active";
+                        } ?>"><a href="<?php echo base_url() . 'dashboard'; ?>"><i class="fa fa-tachometer"></i> <span>Dashboard</span></a></li>
+            <li class="<?php if ($page == "Wisata") {
+                            echo "active";
+                        } ?>"><a href="<?php echo base_url() . 'wisata'; ?>"><i class="fa fa-suitcase"></i> <span>Wisata</span></a></li>
 
-            <li class="<?php if ($page == "Hotel") {echo "active";}?>"><a href="<?php echo base_url() . 'hotel'; ?>"><i class="fa fa-hotel"></i> <span>Hotel</span></a></li>
+            <li class="<?php if ($page == "Hotel") {
+                            echo "active";
+                        } ?>"><a href="<?php echo base_url() . 'hotel'; ?>"><i class="fa fa-hotel"></i> <span>Hotel</span></a></li>
 
-            <li class="<?php if ($page == "Kuliner") {echo "active";}?>"><a href="<?php echo base_url() . 'kuliner'; ?>"><i class="fa fa-cutlery"></i> <span>Kuliner</span></a></li>
+            <li class="<?php if ($page == "Kuliner") {
+                            echo "active";
+                        } ?>"><a href="<?php echo base_url() . 'kuliner'; ?>"><i class="fa fa-cutlery"></i> <span>Kuliner</span></a></li>
 
 
-            <li class="treeview <?php if ($pageTre == "master") {echo "active";}?>">
+            <li class="treeview <?php if ($pageTre == "Master") {
+                                    echo "active";
+                                } ?>">
                 <a href="#">
                     <i class="fa fa-table"></i> <span>Data Master</span>
                     <span class="pull-right-container">
@@ -33,10 +47,18 @@
                     </span>
                 </a>
                 <ul class="treeview-menu">
-                    <li <?php if ($page == "Kuliner") {echo "active";}?>><a href="<?php echo base_url() . 'provinsi'; ?>"><i class="fa fa-circle-o"></i>Provinsi</a></li>
-                    <li <?php if ($page == "kabupaten") {echo "active";}?>><a href="<?php echo base_url() . 'kabupaten'; ?>"><i class="fa fa-circle-o"></i>Kabupaten</a></li>
-                    <li <?php if ($page == "kota") {echo "active";}?>><a href="<?php echo base_url() . 'kota'; ?>"><i class="fa fa-circle-o"></i>Kota</a></li>
-                    <li <?php if ($page == "Kuliner") {echo "active";}?>><a href="<?php echo base_url() . 'user'; ?>"><i class="fa fa-circle-o"></i>Pengguna</a></li>
+                    <li <?php if ($page == "Provinsi") {
+                            echo "active";
+                        } ?>><a href="<?php echo base_url() . 'provinsi'; ?>"><i class="fa fa-circle-o"></i>Provinsi</a></li>
+                    <li <?php if ($page == "Kabupaten") {
+                            echo "active";
+                        } ?>><a href="<?php echo base_url() . 'kabupaten'; ?>"><i class="fa fa-circle-o"></i>Kabupaten</a></li>
+                    <li <?php if ($page == "Kota") {
+                            echo "active";
+                        } ?>><a href="<?php echo base_url() . 'kota'; ?>"><i class="fa fa-circle-o"></i>Kota</a></li>
+                    <li <?php if ($page == "User") {
+                            echo "active";
+                        } ?>><a href="<?php echo base_url() . 'user'; ?>"><i class="fa fa-circle-o"></i>Pengguna</a></li>
                 </ul>
             </li>
         </ul>
